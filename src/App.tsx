527c39296799e0c8da53610d1547dccaf9edce43
Marcos vinicius <marcosvinicius.udia1256@gmail.com>
2020-07-15 15:10:18 -0300

    terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider , DefaultTheme} from "styled-components"
+import light from "./styles/themes/light"
+import dark from "./styles/themes/dark"
+import Global from "./styles/global";
+import Header from "./components/Header";
+import usePersitedState from "./utils/usePersitedState";
+
 
 function App() {
+
+  const [theme, setTheme] = usePersitedState<DefaultTheme>("theme",light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title==="light"? dark: light);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme}>
+       <div>
+      <Global/>
+      <Header toggleTheme={toggleTheme} />
     </div>
+    </ThemeProvider>
+   
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React,{useContext} from "react";
+
+import Switch from "react-switch";
+import {shade}from "polished"
+
+import { Container } from "./styles";
+import { ThemeContext } from "styled-components";
+
+interface Props{
+  toggleTheme():void;
+}
+
+
+const Header: React.FC<Props> = ({toggleTheme}) => {
+  const { colors, title } = useContext(ThemeContext);
+  return (
+    <Container>
+      Hello world
+      <Switch
+        onChange={toggleTheme}
+        checked={title==="dark"}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.1,colors.primary)}
+        onColor={colors.secundary}
+      />
+    </Container>
+  );
+};
+
+export default Header;
